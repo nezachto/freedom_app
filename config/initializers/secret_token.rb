a6803a0fa237a558b8f2cfdc5176fe8a008e87cf
Allen Lao <ineedaslice@example.com>
2013-05-25 10:47:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreedomApp::Application.config.secret_token = 'f5c264fcd9b04b021985fa343f6196be7fbba32d9eed3a667f4f725f881b320128fa9448ad0a6c5dbb9e12b1c8fd1917994aaad98623bef8a30b5d1f93faf4d6'
