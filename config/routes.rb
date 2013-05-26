c7e13294637f56dd1a28fd2d9e897960d2e09247
Allen Lao <ineedaslice@example.com>
2013-05-25 17:49:46 -0700

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FreedomApp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
