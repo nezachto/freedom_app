906f0712cde512f7831b8c6c5dd102a3fd75c3e1
Allen Lao <ineedaslice@example.com>
2014-03-03 13:02:01 -0800

    Added font-face for Candela, also typeplate and normalize css files

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module FreedomApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # add app/assets/fonts to the asset path
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ FreedomApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Disable compile of assets, do everything explicitly
+  config.assets.compile = true
 end
